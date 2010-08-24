7ef3cad62651008353404c6e3331243a9de88cbb
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-08-24 23:56:07 +0000

    Fixes Issue 1440
    Added property callIdAffinityGroupFailover to specify the behavior after a failure. Set to true for buddy replication if you have enough memory in the least busy backup node to handle all calls grouped together.
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/tools/sip-balancer@13779 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- jar/src/main/java/org/mobicents/tools/sip/balancer/CallIDAffinityBalancerAlgorithm.java
+++ jar/src/main/java/org/mobicents/tools/sip/balancer/CallIDAffinityBalancerAlgorithm.java
@@ -3,6 +3,7 @@ package org.mobicents.tools.sip.balancer;
 import gov.nist.javax.sip.header.SIPHeader;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,12 +21,15 @@ import javax.sip.message.Response;
 public class CallIDAffinityBalancerAlgorithm extends DefaultBalancerAlgorithm {
 	private static Logger logger = Logger.getLogger(CallIDAffinityBalancerAlgorithm.class.getCanonicalName());
 	
-	private String headerName = "Call-ID";
-	private ConcurrentHashMap<String, SIPNode> callIdMap = new ConcurrentHashMap<String, SIPNode>();
-	private ConcurrentHashMap<String, Long> callIdTimestamps = new ConcurrentHashMap<String, Long>();
-	private AtomicInteger nextNodeCounter = new AtomicInteger(0);
-	private int maxCallIdleTime = 500;
-	private Timer cacheEvictionTimer = new Timer();
+	protected String headerName = "Call-ID";
+	protected ConcurrentHashMap<String, SIPNode> callIdMap = new ConcurrentHashMap<String, SIPNode>();
+	protected ConcurrentHashMap<String, Long> callIdTimestamps = new ConcurrentHashMap<String, Long>();
+	protected AtomicInteger nextNodeCounter = new AtomicInteger(0);
+	protected int maxCallIdleTime = 500;
+	protected boolean groupedFailover = false;
+	
+	protected Timer cacheEvictionTimer = new Timer();
+	
 	public void processInternalRequest(Request request) {
 		logger.fine("internal request");
 	}
@@ -59,11 +63,21 @@ public class CallIDAffinityBalancerAlgorithm extends DefaultBalancerAlgorithm {
 				if(logger.isLoggable(Level.FINEST)) {
 		    		logger.finest("The assigned node has died. This is the dead node: " + node);
 		    	}
-				node = nextAvailableNode();
-				if(node == null) return null;
-				callIdMap.put(callId, node);
+				if(groupedFailover) {
+					// This will occur very rarely because we re-assign all calls from the dead node in
+					// a single operation
+					SIPNode oldNode = node;
+					node = leastBusyTargetNode(oldNode);
+					if(node == null) return null;
+					groupedFailover(oldNode, node);
+				} else {
+					node = nextAvailableNode();
+					if(node == null) return null;
+					callIdMap.put(callId, node);
+				}
+				
 				if(logger.isLoggable(Level.FINEST)) {
-		    		logger.finest("SO, we must select new node: " + node);
+		    		logger.finest("So, we must select new node: " + node);
 		    	}
 			} else { // ..else it's alive and we can route there
 				//.. and we just leave it like that
@@ -85,12 +99,39 @@ public class CallIDAffinityBalancerAlgorithm extends DefaultBalancerAlgorithm {
 		
 	}
 	
-	private synchronized SIPNode nextAvailableNode() {
+	protected synchronized SIPNode nextAvailableNode() {
 		BalancerContext balancerContext = getBalancerContext();
 		if(balancerContext.nodes.size() == 0) return null;
 		int nextNode = nextNodeCounter.incrementAndGet();
 		nextNode %= balancerContext.nodes.size();
 		return balancerContext.nodes.get(nextNode);
+	}
+	
+	protected synchronized SIPNode leastBusyTargetNode(SIPNode deadNode) {
+		BalancerContext balancerContext = getBalancerContext();
+		HashMap<SIPNode, Integer> nodeUtilization = new HashMap<SIPNode, Integer>();
+		for(SIPNode node : callIdMap.values()) {
+			Integer n = nodeUtilization.get(node);
+			if(n == null) {
+				nodeUtilization.put(node, 0);
+			} else {
+				nodeUtilization.put(node, n+1);
+			}
+		}
+		
+		int minUtil = Integer.MAX_VALUE;
+		SIPNode minUtilNode = null;
+		for(SIPNode node : nodeUtilization.keySet()) {
+			Integer util = nodeUtilization.get(node);
+			if(!node.equals(deadNode) && (util < minUtil)) {
+				minUtil = util;
+				minUtilNode = node;
+			}
+		}
+
+		logger.info("Least busy node selected " + minUtilNode + " with " + minUtil + " calls");
+		
+		return minUtilNode;
 	}
 
 	public void init() {
@@ -99,60 +140,104 @@ public class CallIDAffinityBalancerAlgorithm extends DefaultBalancerAlgorithm {
 			this.maxCallIdleTime = Integer.parseInt(maxTimeInCacheString);
 		}
 		logger.info("Call Idle Time is " + this.maxCallIdleTime + " seconds. Inactive calls will be evicted.");
+		
+		final CallIDAffinityBalancerAlgorithm thisAlgorithm = this;
 		this.cacheEvictionTimer.schedule(new TimerTask() {
 
 			@Override
 			public void run() {
 				try {
-					ArrayList<String> oldCalls = new ArrayList<String>();
-					Iterator<String> keys = callIdTimestamps.keySet().iterator();
-					while(keys.hasNext()) {
-						String key = keys.next();
-						long time = callIdTimestamps.get(key);
-						if(System.currentTimeMillis() - time > 1000*maxCallIdleTime) {
-							oldCalls.add(key);
+					synchronized (thisAlgorithm) {
+						ArrayList<String> oldCalls = new ArrayList<String>();
+						Iterator<String> keys = callIdTimestamps.keySet().iterator();
+						while(keys.hasNext()) {
+							String key = keys.next();
+							long time = callIdTimestamps.get(key);
+							if(System.currentTimeMillis() - time > 1000*maxCallIdleTime) {
+								oldCalls.add(key);
+							}
 						}
-					}
-					for(String key : oldCalls) {
-						callIdMap.remove(key);
-						callIdTimestamps.remove(key);
-					}
-					if(oldCalls.size()>0) {
-						logger.info("Reaping idle calls... Evicted " + oldCalls.size() + " calls.");
-					}
+						for(String key : oldCalls) {
+							callIdMap.remove(key);
+							callIdTimestamps.remove(key);
+						}
+						if(oldCalls.size()>0) {
+							logger.info("Reaping idle calls... Evicted " + oldCalls.size() + " calls.");
+						}}
 				} catch (Exception e) {
 					logger.log(Level.WARNING, "Failed to clean up old calls. If you continue to se this message frequestly and the memory is growing, report this problem.", e);
 				}
+
 			}
 		}, 0, 6000);
-
+		
+		String groupFailoverProperty = getProperties().getProperty("callIdAffinityGroupFailover");
+		if(groupFailoverProperty != null) {
+			this.groupedFailover = Boolean.parseBoolean(groupFailoverProperty);
+		}
+		logger.info("Grouped failover is set to " + this.groupedFailover);
 	}
 	
 	public void assignToNode(String id, SIPNode node) {
 		callIdMap.put(id, node);
 		callIdTimestamps.put(id, System.currentTimeMillis());
 	}
-	
+
 	@Override
 	public void jvmRouteSwitchover(String fromJvmRoute, String toJvmRoute) {
-		SIPNode oldNode = getBalancerContext().jvmRouteToSipNode.get(fromJvmRoute);
-		SIPNode newNode = getBalancerContext().jvmRouteToSipNode.get(toJvmRoute);
-		if(oldNode != null && newNode != null) {
-			int updatedRoutes = 0;
-			for(String key : callIdMap.keySet()) {
-				SIPNode n = callIdMap.get(key);
-				if(n.equals(oldNode)) {
-					callIdMap.replace(key, newNode);
-					updatedRoutes++;
+		try {
+			SIPNode oldNode = getBalancerContext().jvmRouteToSipNode.get(fromJvmRoute);
+			SIPNode newNode = getBalancerContext().jvmRouteToSipNode.get(toJvmRoute);
+			if(oldNode != null && newNode != null) {
+				int updatedRoutes = 0;
+				for(String key : callIdMap.keySet()) {
+					SIPNode n = callIdMap.get(key);
+					if(n.equals(oldNode)) {
+						callIdMap.replace(key, newNode);
+						updatedRoutes++;
+					}
+				}
+				if(logger.isLoggable(Level.INFO)) {
+					logger.info("Switchover occured where fromJvmRoute=" + fromJvmRoute + " and toJvmRoute=" + toJvmRoute + " with " + 
+							updatedRoutes + " updated routes.");
+				}
+			} else {
+				if(logger.isLoggable(Level.INFO)) {
+					logger.info("Switchover failed where fromJvmRoute=" + fromJvmRoute + " and toJvmRoute=" + toJvmRoute);
 				}
 			}
-			if(logger.isLoggable(Level.INFO)) {
-				logger.info("Switchover occured where fromJvmRoute=" + fromJvmRoute + " and toJvmRoute=" + toJvmRoute + " with " + 
-						updatedRoutes + " updated routes.");
-			}
-		} else {
+		} catch (Throwable t) {
 			if(logger.isLoggable(Level.INFO)) {
 				logger.info("Switchover failed where fromJvmRoute=" + fromJvmRoute + " and toJvmRoute=" + toJvmRoute);
+				logger.log(Level.INFO, "This is not a fatal failure, logging the reason for the failure ", t);
+			}
+		}
+	}
+
+	synchronized public void groupedFailover(SIPNode oldNode, SIPNode newNode) {
+		try {
+			if(oldNode != null && newNode != null) {
+				int updatedRoutes = 0;
+				for(String key : callIdMap.keySet()) {
+					SIPNode n = callIdMap.get(key);
+					if(n.equals(oldNode)) {
+						callIdMap.replace(key, newNode);
+						updatedRoutes++;
+					}
+				}
+				if(logger.isLoggable(Level.INFO)) {
+					logger.info("Switchover occured where oldNode=" + oldNode + " and newNode=" + newNode + " with " + 
+							updatedRoutes + " updated routes.");
+				}
+			} else {
+				if(logger.isLoggable(Level.INFO)) {
+					logger.info("Switchover failed where fromJvmRoute=" + oldNode + " and toJvmRoute=" + newNode);
+				}
+			}
+		} catch (Throwable t) {
+			if(logger.isLoggable(Level.INFO)) {
+				logger.info("Switchover failed where fromJvmRoute=" + oldNode + " and toJvmRoute=" + newNode);
+				logger.log(Level.INFO, "This is not a fatal failure, logging the reason for the failure ", t);
 			}
 		}
 	}
