f050289b9959ab3eb1ccf4887fa80f1a03e0ef97
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-10-04 17:12:38 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/tools/sip-balancer@14297 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sipp_scenarios/performance/prepare-and-start-lb.sh
+++ sipp_scenarios/performance/prepare-and-start-lb.sh
@@ -1,4 +1,4 @@
 #mvn clean install -f ../../pom.xml -Dmaven.test.skip=true
 rm -rf logs
 mkdir logs
-java -Djava.util.logging.config.file=./lb-logging.properties -jar ${M2_REPO}/org/mobicents/tools/sip-balancer/1.0.BETA8-SNAPSHOT/sip-balancer-1.0.BETA8-SNAPSHOT-jar-with-dependencies.jar -mobicents-balancer-config=lb-configuration.properties -Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode
+java -Djava.util.logging.config.file=./lb-logging.properties -jar ${M2_REPO}/org/mobicents/tools/sip-balancer/1.0.BETA8-SNAPSHOT/sip-balancer-1.0.BETA8-SNAPSHOT-jar-with-dependencies.jar -mobicents-balancer-config=lb-configuration.properties -Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false
