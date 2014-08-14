220c4ad1b3196a5f19dc315e93d5f6212de8e563
George Vagenas <gvagenas@telestax.com>
2014-08-14 15:48:35 +0300

    LB-13 #Comment work in progress

---
+++ telestax-proxy/extra-resources/create-mariadb.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS proxy;
+USE proxy;
+
+CREATE TABLE restcomm_instances (
+id VARCHAR(20) NOT NULL PRIMARY KEY,
+udpInterface VARCHAR(50),
+tcpInterface VARCHAR(50),
+tlsInterface VARCHAR(50),
+wsInterface VARCHAR(50),
+dateCreated DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS phone_numbers (
+did VARCHAR(20) NOT NULL PRIMARY KEY,
+restcomm_instance VARCHAR(20) NOT NULL,
+dateCreated DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS call_records (
+did VARCHAR(20) NOT NULL PRIMARY KEY,
+dateCreated DATETIME NOT NULL
+);
--- telestax-proxy/src/main/java/org/mobicents/tools/telestaxproxy/sip/balancer/BalancerRunner.java
+++ telestax-proxy/src/main/java/org/mobicents/tools/telestaxproxy/sip/balancer/BalancerRunner.java
@@ -19,14 +19,6 @@
 
 package org.mobicents.tools.telestaxproxy.sip.balancer;
 
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.xml.DOMConfigurator;
-import org.mobicents.tools.sip.balancer.InvocationContext;
 
 /**
  * @author jean.deruelle@gmail.com
@@ -35,65 +27,13 @@ import org.mobicents.tools.sip.balancer.InvocationContext;
  */
 public class BalancerRunner extends org.mobicents.tools.sip.balancer.BalancerRunner implements BalancerRunnerMBean {
 
-    ConcurrentHashMap<String, InvocationContext> contexts = new ConcurrentHashMap<String, InvocationContext>();
-    static {
-        String logLevel = System.getProperty("logLevel", "INFO");
-        String logConfigFile = System.getProperty("logConfigFile");
-
-        if(logConfigFile == null) {
-            Logger.getRootLogger().addAppender(new ConsoleAppender(
-                    new PatternLayout("%r (%t) %p [%c{1}%x] %m%n")));
-            Logger.getRootLogger().setLevel(Level.toLevel(logLevel));
-        } else {
-            DOMConfigurator.configure(logConfigFile);
-        }
-    }
-
-    public InvocationContext getInvocationContext(String version) {
-        if(version == null) version = "0";
-        InvocationContext ct = contexts.get(version);
-        if(ct==null) {
-            ct = new InvocationContext(version, balancerContext);
-            contexts.put(version, ct);
-        }
-        return ct;
-    }
-    public InvocationContext getLatestInvocationContext() {
-        return getInvocationContext(reg.getLatestVersion());
-    }
-
-    private static Logger logger = Logger.getLogger(BalancerRunner.class);
 
     public static void main(String[] args) {
-        logger.info("Starting Telestax Proxy");
-//        if (args.length < 1) {
-//            logger.error("Please specify mobicents-balancer-config argument. Usage is : java -DlogConfigFile=./lb-log4j.xml -jar telestax-proxy-jar-with-dependencies.jar -mobicents-balancer-config=lb-configuration.properties");
-//            return;
-//        }
-//
-//        if(!args[0].startsWith("-mobicents-balancer-config=")) {
-//            logger.error("Impossible to find the configuration file since you didn't specify the mobicents-balancer-config argument. Usage is : java -DlogConfigFile=./lb-log4j.xml -jar telestax-proxy-jar-with-dependencies.jar -mobicents-balancer-config=lb-configuration.properties");
-//            return;
-//        }
+        System.out.println("Starting Telestax Proxy");
         
         // Configuration file Location
         String configurationFileLocation = args[0].substring("-mobicents-balancer-config=".length());
         BalancerRunner balancerRunner = new BalancerRunner();
         balancerRunner.start(configurationFileLocation); 
-    }
-}
-@SuppressWarnings("unused")
-class SipBalancerShutdownHook extends Thread {
-    private static Logger logger = Logger.getLogger(SipBalancerShutdownHook.class
-            .getCanonicalName());
-    BalancerRunner balancerRunner;
-
-    public SipBalancerShutdownHook(BalancerRunner balancerRunner) {
-        this.balancerRunner = balancerRunner;
-    }
-
-    @Override
-    public void run() {
-        balancerRunner.stop();
     }
 }
