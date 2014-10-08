8d8e05c9788f90411908871a795f48623015b96d
Jean Deruelle <jean.deruelle@telestax.com>
2014-10-08 20:56:57 +0200

    LB-25 #Resolve-Issue Fixed #comment fixed prop to avoid mem leaks

--- sipp_scenarios/performance/performance-test.sh
+++ sipp_scenarios/performance/performance-test.sh
@@ -4,6 +4,6 @@ killall sipp
 echo "Performance Testing"
 ./sipp 127.0.0.1:5065 -sf performance-proxy-uas.xml -trace_err -i 127.0.0.1 -p 5090 -nd -bg 
 ./sipp 127.0.0.1:5065 -sf performance-proxy-uas.xml -trace_err -i 127.0.0.1 -p 5091 -nd -bg 
-./sipp 127.0.0.1:5060 -sf performance-proxy-uac.xml -trace_err -i 127.0.0.1 -p 5055 -nd -r 1 -m 10000000
+./sipp 127.0.0.1:5060 -sf performance-proxy-uac.xml -trace_err -i 127.0.0.1 -p 5055 -nd -r 1 -m 5000000
 sleep 10
 killall sipp
