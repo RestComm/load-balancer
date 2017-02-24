32677e560abf20782d563406e5bdaf5e4ccf15db
knosach <kostyantyn.nosach@telestax.com>
2017-02-24 11:59:14 +0200

    fix new test add port for heartbeat

--- jar/src/test/java/org/mobicents/tools/sip/balancer/operation/IncorrectAddRecivedInResponseTest.java
+++ jar/src/test/java/org/mobicents/tools/sip/balancer/operation/IncorrectAddRecivedInResponseTest.java
@@ -66,7 +66,7 @@ public class IncorrectAddRecivedInResponseTest {
 		
 		for(int q=0;q<servers.length;q++)
 		{
-			servers[q] = new AppServer("node" + q,4060+q , "127.0.0.1", 2000, 5060, 5065, "0", ListeningPoint.TCP);			
+			servers[q] = new AppServer("node" + q,4060+q , "127.0.0.1", 2000, 5060, 5065, "0", ListeningPoint.TCP, 2222+q);			
 			servers[q].start();		
 		}
 		Thread.sleep(3000);
